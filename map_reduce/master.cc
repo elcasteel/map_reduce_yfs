c800a58dd44ba4f674dff56b4bfa18c5724b1c12
Kelly <kcasteel@mit.edu>
2011-05-02 20:16:20 -0400

    implement abstract part of master

--- map_reduce/master.cc
+++ map_reduce/master.cc
@@ -1,11 +1,13 @@
 #include "master.h"
 #include <string>
 #include "node.h"
+#include "dirent.h"
 
 master::master(class config *c){
   cfg = c;
   pthread_mutex_init(&map_m);
-
+  pthread_cond_init(&all_mappers_done);
+  pthread_cond_init(&all_reducers_done);
 }
 
 void 
@@ -23,19 +25,133 @@ master::map_reduce(std::string input_file, std::string output_file){
 
   }
   
-  //start mappers
-  start_mappers();
-  //wait on jobs to finish
+  int acquire_delay = 30;
+  while(mapper_nodes.size())
+  {
+     //start mappers
+     start_mappers();
+     //wait on jobs to finish
+     timespec tp;
+     clock_gettime(CLOCK_REALTIME, &tp);
+     acquire_tp.tv_sec += acquire_delay;
+     acquire_delay = acquire_delay *2;
+     pthread_cond_timedwait(&all_mappers_done,&map_m,&tp);
   
-
+  }
   //start reducers
+  acquire_delay = 30;
+  while(reducer_nodes.size())
+  {
+     //start reducers
+     start_reducers();
+     //wait on jobs to finish
+     timespec tp;
+     clock_gettime(CLOCK_REALTIME, &tp);
+     acquire_tp.tv_sec += acquire_delay;
+     acquire_delay = acquire_delay *2;
+     pthread_cond_timedwait(&all_reducers_done,&map_m,&tp);
+  
+  }
 
-
-  //wait on reducers to finish
-
+  
+   pthread_mutex_unlock(&map_m);
 
   //return
 
 
 
 }
+
+void 
+master::start_mappers()
+{
+    //assume we are already holding map_mutex
+    std::map <unsigned,std::string> ::iterator it;
+    for(it=mapper_nodes.begin(); it!= mapper_nodes.end();it++)
+    {
+         //choose a random node
+         std::string node_id =getMember();
+
+         //ask the node to start a new map job 
+         handle h(node_id);
+         rpcc *cl = h.safebind();
+         unsigned key =*it;
+         std::string file = mapper_nodes[key];
+         pthread_mutex_unlock(&map_m);
+         if(cl){
+            ret = cl->call(node::JOB,node::MAP, file,key, cfg->myaddr());
+         } else {
+             printf("bind() failed\n");
+          }
+         pthread_mutex_lock(&map_m);
+
+    }
+}
+
+void 
+master::start_reducers()
+{
+    //assume we are already holding map_mutex
+    std::map <unsigned,std::string> ::iterator it;
+    for(it=reducer_nodes.begin(); it!= reducer_nodes.end();it++)
+    {
+         //choose a random node
+         std::string node_id =getMember();
+
+         //ask the node to start a new map job 
+         handle h(node_id);
+         rpcc *cl = h.safebind();
+         unsigned key =*it;
+         std::string file = reducer_nodes[key];
+         pthread_mutex_unlock(&map_m);
+         if(cl){
+            ret = cl->call(node::JOB,node::REDUCE, file,key, cfg->myaddr());
+         } else {
+             printf("bind() failed\n");
+          }
+         pthread_mutex_lock(&map_m);
+
+    }
+}
+int 
+master::mapper_done(unsigned job_id, std::string intermediate_dir)
+{
+    pthread_mutex_lock(&map_m);
+    if(mapper_nodes.find(job_id) != mapper_nodes.end())
+    {
+         //look up the directory this mapper was writing to
+         DIR *Dir;
+         struct dirent *DirEntry;
+         Dir = opendir(intermediate_directory);
+ 
+         //for each file in the directory, add the file to the list of files for the reducer working on that key
+         while(DirEntry= readdir(Dir))
+         {
+            reducer_nodes[DirEntry->d_name].append(intermediate_dir+"/"+DirEntry->d_name+"\n");
+         }
+
+	//remove this job from the mapper_nodes map
+        mapper_nodes.erase(job_id);
+    }
+    if(mapper_nodes.empty())
+        pthread_cond_signal(&all_mappers_done); 
+    pthread_mutex_unlock(&map_m);
+    return 0;
+}
+//called by node whenever a reducer finishes
+int 
+master::reducer_done(std::string job_id,std::string output_file)
+{
+     pthread_mutex_lock(&map_m);
+     if(reducer_nodes.find(job_id) != reducer_nodes.end())
+     {
+	 reducer_outputs[job_id] = output_file;
+         reducer_nodes.erase(job_id);
+     }
+     if(reducers_nodes.empty())
+         pthread_signal_cond(&all_reducers_done);
+     pthread_mutex_unlock(&map_m);
+     return 0;
+}
+
+
--- map_reduce/master.h
+++ map_reduce/master.h
@@ -7,21 +7,23 @@ class master{
 
 protected:
 class config *cfg;
-virtual void merge(std::string input_dir, std::string output_file);
+virtual void merge(std::string output_file);
 unsigned calls;
 time_t start_time;
 void start_mappers();
 void start_reducers();
 //receive done message from a mapper
-int mapper_done(unsigned job_id);
+int mapper_done(unsigned job_id, std::string intermediate_dir);
 //receive done message from a reducer
-int reducer_done(unsigned job_id);
+int reducer_done(std::string job_id,std::string output_file);
 pthread_cond_t all_mappers_done;
 pthread_cond_t all_reducers_done;
 //map job_id -> input_dir
 std::map <unsigned,std::string>mapper_nodes;
 //map job_id -> input_file (key)
-std::map <unsigned,std::string>reducer_nodes;
+std::map <std::string,std::string>reducer_nodes;
+//map key -> output file
+std::map <unsigned, std::string> reducer_outputs;
 input_reader reader;
 virtual input_reader get_input_reader(std::string input_dir);
 pthread_mutex_t map_m;
@@ -30,4 +32,11 @@ public:
 void map_reduce(std::string input_file, std::string output_file);
 master(class config *c);
 std::string getMember();
-}
+};
+
+class sort_master{
+protected:
+   input_reader get_input_reader(std::string input_dir);
+   void merge(std::string output_file);
+    
+};
