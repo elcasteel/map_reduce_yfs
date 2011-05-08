8f95bb360d865d32f9c58b3bb7bd182711bdfe2f
Sebastien <sibtx13@hotmail.com>
2011-05-08 17:56:04 -0400

    forgot to save node.cc

--- map_reduce/node.cc
+++ map_reduce/node.cc
@@ -1,7 +1,16 @@
 #include "node.h"
 
 
-node::node(){
-  
+node::node(std::string first,std::string me){
+  //init config
+  myid = me;
+  primary = first;
+  cfg = new config(first,me,this);
 
+  //init listener thread
+  
+  //init outgoing thread for async map and reduce rpcs
+
+  //init mutexes, etc
+  pthread_mutex_init(&view_mutex,NULL);
 }
